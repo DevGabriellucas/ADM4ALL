f02cfa9cdeae51ce9b06fb1924fcebeafc097478
Doglaska <doglaska@gmail.com>
2026-05-29 11:17:04 -0300

    feature: add components no forms cadastro

--- frontend/src/app/cadastro/page.tsx
+++ frontend/src/app/cadastro/page.tsx
@@ -1,30 +1,32 @@
 'use client'
 
 import { useState } from "react";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
 
 export default function Cadastro(){
 
-    const[checkbox,setCheckbox] =useState(true)
+    const[checkbox,setCheckbox] =useState(true) //Se o checkbox constar marcado aparece os inputs internos, else nao aparece
 
     return (
-        <div className="flex min-h-screen items-center justify-center">
+        <main className="flex min-h-screen w-full flex-col items-center justify-center gap-y-16 bg-linear-to-bl from-brand-dark/90 via-brand-medium/90 to-brand-light/90 p-4 font-poppins xl:flex-row xl:gap-x-20 xl:gap-y-0">
             <form className="flex flex-col gap-4 w-96 p-6 border rounded-lg mt-5 mb-5 shadow-md">
                 <h1 className="flex items-center justify-center">Cadastro</h1>
-                <input type="text" className="border rounded-lg p-2" placeholder="Nome"/>
-                <input type="text" className="border rounded-lg p-2" placeholder="CPF"/>
-                <input type="number" className="border rounded-lg p-2" placeholder="Telefone: (83) 91234-1234"/>
-                <input type="date" className="border rounded-lg p-2"/>
-                <input type="email" className="border rounded-lg p-2" placeholder="email"/>
-                <input type="password" className="border rounded-lg p-2" placeholder="Senha"/>
+                <Input type="text" className="border rounded-lg p-2" placeholder="Nome"/>
+                <Input type="text" className="border rounded-lg p-2" placeholder="CPF"/>
+                <Input type="number" className="border rounded-lg p-2" placeholder="Telefone: (83) 91234-1234"/>
+                <Input type="date" className="border rounded-lg p-2"/>
+                <Input type="email" className="border rounded-lg p-2" placeholder="email"/>
+                <Input type="password" className="border rounded-lg p-2" placeholder="Senha"/>
         
                 <div className="flex flex-col">
                     <label>Aluno da UNIPÊ</label>
-                    <input type="checkbox" checked={checkbox} onChange={() => setCheckbox(!checkbox)}/>
+                    <Input type="checkbox" checked={checkbox} onChange={() => setCheckbox(!checkbox)}/>
                 </div>
                     
                 {checkbox && (
                     <div className="flex flex-col gap-4">
-                        <input type="number" className="border rounded-lg p-2" placeholder="RGM"/>
+                        <Input type="number" className="border rounded-lg p-2" placeholder="RGM"/>
                         <select className="border rounded-lg p-2">
                             <option value="Administração">Administração</option>
                             <option value="Análise e Desenvolvimento de Sistemas">Análise e Desenvolvimento de Sistemas</option>
@@ -40,9 +42,9 @@ export default function Cadastro(){
                     <option value="">Empreendedorismo</option>
                 </select>
 
-                <button type="submit" className="border rounded-md p-2 bg-blue-400">Cadastrar</button>
+                <Button type="submit" className="bg-brand-dark text-white p-2 rounded-lg">Cadastrar</Button>
 
             </form>
-        </div>
+        </main>
     );
 }
