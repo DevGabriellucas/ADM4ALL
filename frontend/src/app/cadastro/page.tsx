f3fcacfa75af1dad914231880060752630a29fa8
Doglaska <doglaska@gmail.com>
2026-06-11 13:41:04 -0300

    feat: adicionar hook e zod

--- frontend/src/app/cadastro/page.tsx
+++ frontend/src/app/cadastro/page.tsx
@@ -5,31 +5,51 @@ import { useState } from "react";
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
 import {Select} from "@/components/Select"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  cadastroFormDataSchema,
+  type CadastroFormData
+} from "@/schemas/cadastroSchema";
 
 export default function Cadastro(){
 
     const[checkbox,setCheckbox] =useState(true) //Se o checkbox constar marcado aparece os inputs internos, else nao aparece
 
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<CadastroFormData>({
+        resolver: zodResolver(cadastroFormDataSchema)
+    });
+
+    const cadastroSubmit = (data: CadastroFormData) => {
+        console.log(data);
+    };
+
     return (
         <main className="flex min-h-screen w-full flex-col items-center justify-center gap-y-16 bg-linear-to-bl from-brand-dark/90 via-brand-medium/90 to-brand-light/90 p-4 font-poppins xl:flex-row xl:gap-x-20 xl:gap-y-0">
             <h1 className="flex items-center justify-center text-2xl font-bold">Cadastro</h1>
-                <form className="flex w-full max-w-md flex-col gap-4 rounded-xl bg-[#9FA3C7E5]/60 px-6 py-9 max-w-md">
-                <Input type="text" className="rounded-lg p-2" placeholder="Nome"/>
-                <Input type="text" className="rounded-lg p-2" placeholder="CPF"/>
-                <Input type="tel" className="rounded-lg p-2" placeholder="Telefone: (83) 91234-1234"/>
-                <Input type="date" className="rounded-lg p-2"/>
-                <Input type="email" className="rounded-lg p-2" placeholder="email"/>
+                <form onSubmit={handleSubmit(cadastroSubmit)} 
+                    className="flex w-full max-w-md flex-col gap-4 rounded-xl bg-[#9FA3C7E5]/60 px-6 py-9">
+                <Input type="text" className="rounded-lg p-2" placeholder="Nome" {...register("nome")} error={errors.nome?.message}/>
+                <Input type="text" className="rounded-lg p-2" placeholder="CPF" {...register("cpf")}error={errors.cpf?.message}/>
+                <Input type="tel" className="rounded-lg p-2" placeholder="Telefone" {...register("telefone")} error={errors.telefone?.message}/>
+                <Input type="date" className="rounded-lg p-2" {...register("dataNascimento")}/>
+                <Input type="email" className="rounded-lg p-2" placeholder="email" {...register("email")} error={errors.email?.message}/>
                 <Input type="password" className="rounded-lg p-2" placeholder="Senha"/>
         
                 <div className="flex items-center gap-3">
-                    <Input type="checkbox" checked={checkbox} onChange={() => setCheckbox(!checkbox)}/>
+                    <Input type="checkbox" checked={checkbox} {...register("isAlunoUnipe")} onChange={() => setCheckbox(!checkbox)}/>
                     <label>Aluno da UNIPÊ</label>
                 </div>
                     
                 {checkbox && (
                     <div className="flex flex-col gap-4">
                         <Input type="number" className="rounded-lg p-2" placeholder="RGM"/>
-                        <Select className="w-full max-w-full rounded-lg p-2">
+                        <Select className="w-full max-w-full rounded-lg p-2" {...register("cursoUnipe")}>
                             <option value="ADM">ADM</option>
                             <option value="ADS">ADS</option>
                             <option value="CC">Ciência da Computação</option>
---
+++ frontend/src/schemas/cadastroSchema.ts
@@ -0,0 +1,29 @@
+import { z } from "zod/v4";
+
+export const cadastroFormDataSchema = z.object({
+  nome: z.string().trim().min(3, {
+    error: "Nome inválido!"
+  }),
+
+  cpf: z.string().trim().length(11, {
+    error: "CPF inválido!"
+  }),
+
+  telefone: z.string().trim().min(10, {
+    error: "Telefone inválido!"
+  }),
+
+  email: z.email({
+    error: "Email inválido!"
+  }),
+
+  dataNascimento: z.string(),
+
+  isAlunoUnipe: z.boolean(),
+
+  cursoUnipe: z.string().optional()
+  
+});
+
+export type CadastroFormData =
+  z.infer<typeof cadastroFormDataSchema>;
