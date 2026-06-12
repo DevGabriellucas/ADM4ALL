473f72486bf8be7c2c4c1f2defe537601e4ad50e
Danilo Queiroz <danilof.queiroz1@gmail.com>
2026-06-12 14:35:29 -0300

    refactor: rename 'message' property to 'error' in forgotPasswordSchema

--- frontend/src/schemas/forgotPasswordSchema.ts
+++ frontend/src/schemas/forgotPasswordSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod/v4";
 
 export const forgotPasswordDataSchema = z.object({
   email: z
-    .email({ message: "E-mail inválido!" })
+    .email({ error: "E-mail inválido!" })
 });
 
 export type ForgotPasswordData = z.infer<typeof forgotPasswordDataSchema>;
