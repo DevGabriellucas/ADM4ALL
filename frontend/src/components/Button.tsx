d6425973882058e36945441479f13105d650abcc
Gabriel Lucas <gabrielbandeira2527l@gmail.com>
2026-06-27 12:30:12 -0300

    fix: corrige validacao de cadastro e bugs em Button/Select

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,14 +2,14 @@ import type { ComponentProps } from "react";
 
 interface ButtonProps extends ComponentProps<"button"> {}
 
-export const Button = ({ children, className ,...props }: ButtonProps) => {
+export const Button = ({ children, className, ...props }: ButtonProps) => {
   return (
     <button
-      className={`h-[5.22rem] w-full cursor-pointer rounded-lg bg-radial-[at_0%_48.97%] from-[#78A4EA] to-[#445D84] px-10 py-4 font-semibold text-2xl hover:brightness-110 transition"
+      className={`h-[5.22rem] w-full cursor-pointer rounded-lg bg-radial-[at_0%_48.97%] from-[#78A4EA] to-[#445D84] px-10 py-4 font-semibold text-2xl hover:brightness-110 transition
         ${className}`}
       {...props}
     >
       {children}
     </button>
-  )
-}
+  );
+};
--- frontend/src/components/Select.tsx
+++ frontend/src/components/Select.tsx
@@ -1,15 +1,18 @@
-import type { ComponentProps } from "react";
+import { type ComponentProps, forwardRef } from "react";
 
-interface SelectProps extends ComponentProps <"select"> {}
+interface SelectProps extends ComponentProps<"select"> {}
 
-export const Select = ({children, className, ... props}: SelectProps) => {
+export const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  ({ children, className, ...props }, ref) => {
     return (
-        <select
-            className={`h-[5.22rem] w-full max-w-full overflow-hidden cursor-pointer rounded-lg bg-radial-[at_0%_50.72%] from-[#BFD0EC] to-[#6D7686] px-10 py-4 text-base outline-none placeholder:font-normal placeholder:text-2xl
+      <select
+        ref={ref}
+        className={`h-[5.22rem] w-full max-w-full overflow-hidden cursor-pointer rounded-lg bg-radial-[at_0%_50.72%] from-[#BFD0EC] to-[#6D7686] px-10 py-4 text-base outline-none placeholder:font-normal placeholder:text-2xl
                 ${className}`}
-            {...props}
-        >
-            {children}
-        </select>
-    )
-}
+        {...props}
+      >
+        {children}
+      </select>
+    );
+  },
+);
